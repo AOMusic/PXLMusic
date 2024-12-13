0886b5089d3d79cdb4bc22e4a7f2ce27f9a8eb37
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-13 14:10:09 +0530

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-from telegram.ext import CommandHandler, CallbackContext
-from telegram import Update
-
-def start_command(update: Update, context: CallbackContext):
-    update.message.reply_text("Welcome to our bot!")
-    update.message.reply_text("Type /help for more information.")
-
-start_handler = CommandHandler('start', start_command)
