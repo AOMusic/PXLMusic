c2a0a7cfd86978cbba26142e648ebee9374f610e
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-29 16:50:52 +0530

    Delete Ads.py

--- Ads.py
+++
@@ -1,9 +0,0 @@
-import logging
-from telegram.ext import CallbackContext
-from telegram import Update
-
-logging.basicConfig(level=logging.INFO)
-
-def send_ads(update: Update, context: CallbackContext):
-    ads_text = "Yeh ads hai!"
-    update.message.reply_text(ads_text)
